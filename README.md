cd339debc05902167fe9749f810a0507d8975e3a
cyborgsystems <44999852+cyborgsystems@users.noreply.github.com>
2018-12-11 12:38:34 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react_wizard
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,62 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from "react";
+import ReactDOM from "react-dom";
+  
+class Wizard extends React.Component {
+  constructor(props) {
+    super(props);
+    this._onNext = this._onNext.bind(this);
+    this._onPre = this._onPre.bind(this);
+    this.state = { counter: 0 };
+  }
+
+  _onNext = () => {
+    this.setState({counter:this.state.counter+1})
+  };
+  _onPre = () => {
+    this.setState({ counter: this.state.counter -1 })
+};
+
+  render() {
+    const children = React.Children.map(this.props.children, (child, index) =>
+      React.cloneElement(child, {
+        ref: `child${index}`
+      })
+    );
+    return (
+      <div className="card">
+        <div className="card-body">{children[this.state.counter]}</div>
+      <div className="card-footer">
+      <div style={{float:'right'}}>
+          <button className="btn btn-danger" onClick={this._onPre}>Prev</button>
+      
+          <button className="btn btn-primary" onClick={this._onNext}>Next</button>
+    </div>
+
+
+      </div>
+
+      </div>
+
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Wizard>
+
+        <h1>First</h1>
+        <h1>Second</h1>
+        <h1>Third</h1>
+
+
+      </Wizard>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
